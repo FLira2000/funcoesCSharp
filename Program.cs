134ae893d9d61fc1964ef5eec7238fa131c23a46
Fábio Lira <fabiolira1910@hotmail.com>
2019-10-05 19:49:45 -0300

    jojo
    major upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace funcoes
+{
+    class Program
+    {
+        public static int Soma(int n1, int n2){
+            return n1 + n2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Za Warudo!");
+            Console.WriteLine(Soma(2, 3));
+        }
+    }
+}
